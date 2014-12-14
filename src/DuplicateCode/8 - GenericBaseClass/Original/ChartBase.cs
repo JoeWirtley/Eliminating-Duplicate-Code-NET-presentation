d41c6729d9b6d1ad60f558de4623d48a5be82037
Joe Wirtley <JoeWirtley@users.noreply.github.com>
2014-12-14 12:55:01 -0500

    Renamed classes.

---
+++ src/DuplicateCode/8 - GenericBaseClass/Original/ChartBase.cs
@@ -0,0 +1,21 @@
+﻿using DuplicateCode.GenericBaseClass.Support;
+
+namespace DuplicateCode.GenericBaseClass.Original {
+   public abstract class ChartBase: VisualObject, IChart {
+      public abstract IChartDefCommon ChartDefinition { get; set; }
+      public abstract string Serialize( IChartSerializer chartSerializer );
+      protected abstract ChartBase CreateNewInstance();
+
+      public string Name {
+         get { return ChartDefinition.Name; }
+         set { ChartDefinition.Name = value; }
+      }
+
+      public VisualObject Copy( string newChartName ) {
+         var newChart = CreateNewInstance();
+         newChart.ChartDefinition = ChartDefinition;
+         newChart.ChartDefinition.Name = newChartName;
+         return newChart;
+      }
+   }
+}
---
+++ src/DuplicateCode/8 - GenericBaseClass/Refactored/ChartBase.cs
@@ -0,0 +1,34 @@
+﻿using DuplicateCode.GenericBaseClass.Support;
+
+namespace DuplicateCode.GenericBaseClass.Refactored {
+   public abstract class ChartBase< TChart, TChartDefinition >: VisualObject, IChart
+      where TChart : ChartBase<TChart, TChartDefinition>, new() 
+      where TChartDefinition : IChartDefCommon {
+      private TChartDefinition _chartDef;
+
+      public abstract string Serialize( IChartSerializer chartSerializer );
+
+      public TChartDefinition ChartDefinition {
+         get { return _chartDef; }
+         set { _chartDef = value; }
+      }
+
+      // Explicit interface implementation to support existing code
+      IChartDefCommon IChart.ChartDefinition {
+         get { return _chartDef; }
+      }
+
+      public string Name {
+         get { return ChartDefinition.Name; }
+         set { ChartDefinition.Name = value; }
+      }
+
+      public VisualObject Copy( string newChartName ) {
+         var newChart = new TChart {
+            ChartDefinition = ChartDefinition,
+         };
+         newChart.ChartDefinition.Name = newChartName;
+         return newChart;
+      }
+   }
+}
